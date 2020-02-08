923e129d097ddc466777da04ea3d7ad884b54539
Bartenev Dmitriy <kuhelbeher@gmail.com>
2020-02-08 19:08:52 +0200

    Added some styling

--- App.js
+++ App.js
@@ -4,17 +4,19 @@ import { StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.screenView}>
       <Header title="Guess a number" />
       <StartGameScreen />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   screenView: {
     flex: 1,
   },
 });
+
+export default App;
